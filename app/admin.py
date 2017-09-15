e67a0a914069d4b4c387285efe537ce7f6ef2f0f
zy <744237154@qq.com>
2017-09-16 01:05:21 +0800

    增加后台搜索功能

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from app import models
 # Register your models here.
+class NovelContentAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title']
+
 admin.site.register(models.Novel)
-admin.site.register(models.NovelContent)
+admin.site.register(models.NovelContent, NovelContentAdmin)
 admin.site.register(models.ReadProgress)
--- zynovel/settings.py
+++ zynovel/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
